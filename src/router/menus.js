5a01cd3a9a80797aec447f6d0696f146ae16555b
alex <961227167@qq.com>
2019-04-10 10:50:58 +0800

    修改

--- src/router/menus.js
+++ src/router/menus.js
@@ -3,7 +3,7 @@ export const menus = [
         path: '/user', title: '用户管理', icon: 'team',
         children: [
             { path: '/user/detail', title: '用户列表' },
-            { path: '/user/lottery', title: '中间用户查询' }
+            { path: '/user/lottery', title: '中奖用户查询' }
         ],
     },
     {
